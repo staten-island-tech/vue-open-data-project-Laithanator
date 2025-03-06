63a279ad2b6638b62b2d2bdab739302b87983dc2
Laithanator <144480666+Laithanator@users.noreply.github.com>
2025-03-06 13:05:23 -0500

    5j3k46l487

---
+++ app/src/components/TestCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="border-2 border-black shadow-md w-[30%] h-[40%] flex flex-col items-center p-4">
+    <h1 class="text-2xl">{{ school.location }}</h1>
+  </div>
+</template>
+
+<script setup>
+defineProps(['school'])
+</script>
+
+<style scoped></style>
--- app/src/views/FrontPage.vue
+++ app/src/views/FrontPage.vue
@@ -1,9 +1,23 @@
 <template>
-  <FrontHeader />
+  <main>
+    <FrontHeader />
+    <TestCard v-for="school in schools" :key="school.location" :school="school" />
+  </main>
 </template>
 
 <script setup>
 import FrontHeader from '@/components/FrontHeader.vue'
+import TestCard from '@/components/TestCard.vue'
+import { onMounted, reactive, ref } from 'vue'
+let schools = ref([])
+async function getSchools() {
+  let res = await fetch('https://data.cityofnewyork.us/resource/ven4-h25u.json?$limit=1577')
+  let data = await res.json()
+  schools.value = data
+}
+onMounted(() => {
+  getSchools()
+})
 </script>
 
 <style scoped></style>
